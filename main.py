847b513a4d2d835036928cf2780f75cf25197faa
PRAT-Cantin <cantin.prat@gmail.com>
2024-09-30 10:51:08 +0200

    🗿Preference table generation done🗿

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+def generate_preference_table(attributes, min_max, weights):
+    """
+    Generates the preference table for the problem.
+
+    Args:
+        attributes: The attributes on which the data is being valued
+        min_max: Array of either 1 for max or -1 for min
+        weights: Weight of each attribute
+
+    Returns:
+        pref_table : The preference table of the given values.
+    """
+
+    pref_table = np.zeros((len(attributes),len(attributes)))
+    for i in range(len(attributes)):
+        for j in range(len(attributes[i])):
+            for k in range(len(attributes)):
+                if k == i:
+                    pref_table[i][k] = None
+                    continue
+                if (attributes[i][j]*min_max[j] > attributes[k][j]*min_max[j]):
+                    pref_table[i][k] += weights[j]
+    return pref_table
+
+classes = ["A","B","C"]
+attributes = [[15,16,16],
+              [16,8,7],
+              [13,18,12]]
+min_max = [1,1,1]
+weights = [0.6,0.3,0.1]
+
+pref_table = generate_preference_table(attributes,min_max,weights)
+
+print(pref_table)
+
